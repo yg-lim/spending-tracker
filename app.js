085dc45aa68da0777f2ff512cfea399973315342
qseudo <57642143+yg-lim@users.noreply.github.com>
2023-03-26 17:34:05 +0900

    sorting parameterized routes and dynamic pug view

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ const app = express();
 const PORT = 3000;
 const HOST = "localhost";
 const account = require("./lib/seed-data");
+const expensesNewToOld = require("./lib/expenses-new-to-old");
+const MONTHS_IN_YEAR = {
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December",
+};
 
 app.set("view engine", "pug");
 app.set("views", "./views");
@@ -16,14 +31,25 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   let today = new Date();
-  let yearMonth = `${today.getFullYear()}/${today.getMonth() + 1}`;
-
+  let yearMonth = `${today.getFullYear()}/${today.getMonth()}`;
   res.redirect(yearMonth);
 });
 
 app.get("/:year/:month", (req, res) => {
+  let year = +req.params.year;
+  let month = +req.params.month;
+  let yearMonth = `${year}${month}`;
+  let monthsExpenses = account.expensesByYearMonth(yearMonth);
+
+  // perhaps make a dates object that we can pass into the view
+  // has various props such as Next Month, Prev Month (value generated from a static method we'll add to Account class, we can move the MONTHS_IN_YEAR data structure to it too)
+
   res.render("expenses", {
-    expenses: account.expensesNewToOld(),
+    expenses: expensesNewToOld(monthsExpenses),
+    total: account.totalByMonth(monthsExpenses),
+    monthToString: MONTHS_IN_YEAR[month],
+    month,
+    year,
   });
 });
 
